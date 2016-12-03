bf558ac82c9d3cb1c4bc437969a65b1a44ae05b2
offworldbeacon <david.nibbe@gmail.com>
2016-12-03 15:27:06 -0600

    Update

--- role.harvester.js
+++ role.harvester.js
@@ -14,10 +14,10 @@ var roleHarvester = {
         //Find sapwners and extensions
         spawnextension = creep.pos.findClosestByRange(FIND_STRUCTURES, {
           filter: (structure) => {return (structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION) && structure.energy < structure.energyCapacity;}});
-
+        console.log(spawnextension[0]);
         //Transfer energy to spawner or extension if close enough, otherwise, move closer to spawner or extension
-        if(creep.transfer(spawnextension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(spawnextension);
+        if(creep.transfer(spawnextension[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(spawnextension[0]);
         }
       }
 	}
