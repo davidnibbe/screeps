ac1add20219b41c0fe81a26e561a37ed7bf841e4
offworldbeacon <david.nibbe@gmail.com>
2016-12-03 15:20:24 -0600

    Update

--- role.harvester.js
+++ role.harvester.js
@@ -3,25 +3,23 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function(creep) {
 	    if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
+        //Find all the sources
+        var sources = creep.room.find(FIND_SOURCES);
+        //Harvest if close enough, if not, move closer to source
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(sources[0]);
         }
-        else {
-            spawnextension = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION) && structure.energy < structure.energyCapacity;
-                    }
-            });
-                if(creep.transfer(spawnextension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(spawnextension);
-                }
-	    else {
-                var availflags = creep.room.find(FIND_FLAGS)
-                    creep.moveTo(availflags[creep.memory.assignedSource]);
-            }
+      }
+      else {
+        //Find sapwners and extensions
+        spawnextension = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+          filter: (structure) => {return (structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION) && structure.energy < structure.energyCapacity;}});
+
+        //Transfer energy to spawner or extension if close enough, otherwise, move closer to spawner or extension
+        if(creep.transfer(spawnextension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(spawnextension);
         }
+      }
 	}
 };
 
