ea28fa124514b0be0c2c979021c014b082d1db64
offworldbeacon <david.nibbe@gmail.com>
2016-12-04 14:50:27 -0600

    Update

--- role.repairer.js
+++ role.repairer.js
@@ -22,13 +22,11 @@ var roleRepairer = {
           return (structure.hits < structure.hitsMax / 1.5 )
         }
       });
-      console.log(mytargets)[0];
       var mystorage = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
           return ((structure.structureType == STRUCTURE_CONTAINER) && structure.hits < structure.hitsMax / 1.25 )
         }
       });
-      console.log(mystorage)[0];
       var targets = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
           return (structure.hits < structure.hitsMax / 1.5 )
