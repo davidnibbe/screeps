3bac6e712f1859f5468b8034fb5c803d9ad3db5c
offworldbeacon <david.nibbe@gmail.com>
2016-12-03 19:33:52 -0600

    update

--- role.upgrader.js
+++ role.upgrader.js
@@ -3,26 +3,31 @@ var roleUpgrader = {
     /** @param {Creep} creep **/
     run: function(creep) {
 
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
-            creep.memory.upgrading = false;
-            creep.say('harvesting');
-	    }
-	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
-	        creep.memory.upgrading = true;
-	        creep.say('upgrading');
-	    }
+      if(creep.memory.upgrading && creep.carry.energy == 0) {
+          creep.memory.upgrading = false;
+          creep.say('harvesting');
+      }
+      if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+          creep.memory.upgrading = true;
+          creep.say('upgrading');
+      }
 
-	    if(creep.memory.upgrading) {
+      if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller);
             }
-        }
-        else {
-            var spawn = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
-            if(creep.withdraw(spawn) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(spawn);
+      }
+      else {
+        var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
+        if (creep.pos.isNearTo(spawn)){
+            if (spawn.energy > 199){
+              var transferresult = spawn.transferEnergy(creep);
             }
         }
+        else {
+          creep.moveTo(spawn);
+        }
+      }
 	}
 };
 
