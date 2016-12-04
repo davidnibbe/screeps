51b098482ef0d60116b45e74a34dd2785cbb1492
offworldbeacon <david.nibbe@gmail.com>
2016-12-04 15:17:57 -0600

    Update

--- role.repairer.js
+++ role.repairer.js
@@ -27,11 +27,12 @@ var roleRepairer = {
           return ((structure.structureType == STRUCTURE_CONTAINER) && structure.hits < structure.hitsMax / 1.25 )
         }
       });
-      var targets = creep.room.find(FIND_STRUCTURES, {
+      var allstructures = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
           return (structure.hits < structure.hitsMax / 1.5 )
         }
       });
+      //If there is a structure in my structures then work repair them first
       if (mytargets > 0){
         if (creep.pos.isNearTo(mytargets[0])){
           creep.repair(mytargets[0]);
@@ -42,22 +43,37 @@ var roleRepairer = {
         }
       }
       else{
-        if (mystorage > 0){
-          if (creep.pos.isNearTo(mystorage[0])){
-            creep.repair(mystorage[0]);
+        //if there isn't any structures in my_strucutres then repair storage
+        if (mystorage != null){
+          if (mystorage > 0){
+            if (creep.pos.isNearTo(mystorage[0])){
+              creep.repair(mystorage[0]);
+            }
+            else {
+              moverequired = true;
+              movetarget = mystorage[0];
+            }
           }
-          else {
-            moverequired = true;
-            movetarget = mystorage[0];
+          else{
+            if (creep.pos.isNearTo(mystorage)){
+              creep.repair(mystorage);
+            }
+            else {
+              moverequired = true;
+              movetarget = mystorage;
+            }
           }
         }
+        //if there isn't any storage to repair, repair whatever else you can find
         else{
-          if (creep.pos.isNearTo(targets[0])){
-            creep.repair(targets[0]);
-          }
-          else {
-            moverequired = true;
-            movetarget = targets[0];
+          if (allstructures > 0){
+            if (creep.pos.isNearTo(allstructures[0])){
+              creep.repair(allstructures[0]);
+            }
+            else{
+              moverequired = true;
+              movetarget = allstructures[0];
+            }
           }
         }
       }
