edfc5c789ba0a1e948fa0df1504e3b254f06891e
offworldbeacon <david.nibbe@gmail.com>
2016-12-04 08:39:30 -0600

    Update harvester to account for containers

--- role.harvester.js
+++ role.harvester.js
@@ -14,22 +14,38 @@ var roleHarvester = {
 
         //Find sapwners and extensions
         spawnextension = creep.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                            structure.energy < structure.energyCapacity;
-                    }
-            });
+          filter: (structure) => {
+            return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+              structure.energy < structure.energyCapacity;
+            }
+          });
 
-        if(spawnextension.length > 0){
-          //Transfer energy to spawner or extension if close enough, otherwise, move closer to spawner or extension
-          if(creep.transfer(spawnextension[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(spawnextension[0]);
+        //find containers
+        targetcontainer = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+          filter: (structure) => {
+            return (structure.structureType == STRUCTURE_CONTAINER) && structure.storage(RESOURCE_ENERGY) < structure.storageCapacity;
+          }
+        });
+
+        //if there are containers, transfer to container
+        if(targetcontainer != null){
+          if(creep.transfer(targetcontainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+            creep.moveTo(targetcontainer);
           }
         }
+        //otherwise transfer to spawn or extension
         else{
-          //Transfer energy to spawner or extension if close enough, otherwise, move closer to spawner or extension
-          if(creep.transfer(spawnextension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(spawnextension);
+          if(spawnextension.length > 0){
+            //Transfer energy to spawner or extension if close enough, otherwise, move closer to spawner or extension
+            if(creep.transfer(spawnextension[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(spawnextension[0]);
+            }
+          }
+          else{
+            //Transfer energy to spawner or extension if close enough, otherwise, move closer to spawner or extension
+            if(creep.transfer(spawnextension, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(spawnextension);
+            }
           }
         }
       }
