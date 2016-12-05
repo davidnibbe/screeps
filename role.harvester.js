e34825434af9620bd84a8fdd9e7953079aba73d9
Nibbe <David.Nibbe@securian.com>
2016-12-05 13:55:49 -0600

    updated role.harvester

--- role.harvester.js
+++ role.harvester.js
@@ -6,8 +6,8 @@ var roleHarvester = {
         //Find all the sources
         var sources = creep.room.find(FIND_SOURCES);
         //Harvest if close enough, if not, move closer to source
-        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(sources[0]);
+        if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(sources[1]);
         }
       }
       else {
