7c83300e5ceea88179820b5b3590e82442b839b2
offworldbeacon <david.nibbe@gmail.com>
2016-12-04 21:17:17 -0600

    update builder

--- role.builder.js
+++ role.builder.js
@@ -1,3 +1,4 @@
+var roleUpgrader = require('role.upgrader');
 var roleBuilder = {
 
     /** @param {Creep} creep **/
@@ -15,10 +16,13 @@ var roleBuilder = {
 
 	    if(creep.memory.building) {
 	        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
+            if(targets.length > 0) {
                 if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(targets[0]);
                 }
+            }
+            else{
+              roleUpgrader.run(creep);
             }
 	    }
 	    else {
