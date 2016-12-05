8dc4031a65021d248b774ccfb4c142ddf7ae290d
offworldbeacon <david.nibbe@gmail.com>
2016-12-04 21:14:16 -0600

    update role.repairer

--- role.repairer.js
+++ role.repairer.js
@@ -12,41 +12,17 @@ var roleRepairer = {
       }
     }
     else {
-      var mytargets = creep.room.find(FIND_MY_STRUCTURES, {
-        filter: (structure) => {
-          return (structure.hits < structure.hitsMax / 1.5 )
-        }
-      });
-      var mystorage = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return ((structure.structureType == STRUCTURE_CONTAINER || structure.structureType == STRUCTURE_ROAD) && structure.hits < structure.hitsMax / 1.25 )
-        }
-      });
       var allstructures = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
-          return (structure.hits < structure.hitsMax / 1.5 )
+          return ((structure.structureType != STRUCTURE_WALL) && structure.hits < structure.hitsMax / 1.5 )
         }
       });
-      //If there is a structure in my structures then work repair them first
-      if ((mytargets.length > 0) && (actiontaken == false)){
-        if (creep.repair(mytargets[0]) == ERR_NOT_IN_RANGE){
-          creep.moveTo(mytargets[0]);
-          actiontaken = true
-        }
+      if (creep.repair(allstructures[0]) == ERR_NOT_IN_RANGE){
+        creep.moveTo(allstructures[0]);
+        actiontaken = true
       }
-      //if there isn't any structures in my_strucutres then repair storage
-      if ((mystorage.length > 0) && (actiontaken == false)){
-        if (creep.repair(mystorage[0] == ERR_NOT_IN_RANGE)){
-          creep.moveTo(mystorage[0]);
-          actiontaken = true
-        }
-      }
-      //if there isn't any storage to repair, repair whatever else you can find
-      if ((allstructures.length > 0) && (actiontaken == false)){
-        if (creep.repair(allstructures[0]) == ERR_NOT_IN_RANGE){
-          creep.moveTo(allstructures[0]);
-          actiontaken = true
-        }
+      else{
+        roleBuilder.run(creep);
       }
     }
   }
