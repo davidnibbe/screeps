6d1dddf34a64efd75918790979a8b7a6422b227c
offworldbeacon <david.nibbe@gmail.com>
2016-12-06 14:02:19 -0600

    Revert "Update mover to prefer full storage"
    This reverts commit 6c713cf98db73df90d5e6df0d86c483e39989f06.

--- role.mover.js
+++ role.mover.js
@@ -2,7 +2,6 @@ var roleMover = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-    var actiontaken;
     var targets = creep.room.find(FIND_MY_STRUCTURES, {
       filter: (structure) => {
         return (structure.structureType == STRUCTURE_SPAWN || structure.structureType == STRUCTURE_EXTENSION) &&
@@ -28,16 +27,9 @@ var roleMover = {
       container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
         filter: (structure) => {
           return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > 0}
-      });
-      fullcontainer = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (structure.structureType == STRUCTURE_CONTAINER) && structure.store[RESOURCE_ENERGY] > structure.storeCapacity}
-      });
-      if(creep.withdraw(fullcontainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(fullcontainer);
-        actiontaken = true;
-      }
-	    if(creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE && actiontaken == false) {
+        });
+
+	    if(creep.withdraw(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(container);
       }
     }
