4e18f221d6db137ff1e4b59c6f7e30fdc5282751
offworldbeacon <david.nibbe@gmail.com>
2016-12-06 12:04:12 -0600

    Update wall repairer to calculate percent at .00001

--- role.wallrepairer.js
+++ role.wallrepairer.js
@@ -11,7 +11,7 @@ var roleWallRepairer = {
       }
     });
 
-    for(var percent = .0001; percent < 1; percent + .0001 ){
+    for(var percent = .00001; percent < 1; percent + .00001 ){
 
       for(var i = 0; i < walls.length; i++){
         if(walls[i].hits < walls[i].hitsMax * percent){
