f432caf58919200c8a8ae53b4890c30640f5400a
offworldbeacon <david.nibbe@gmail.com>
2016-12-09 07:05:53 -0600

    recreate wallrepairer

---
+++ role.wallrepairer.js
@@ -0,0 +1,44 @@
+var roleBuilder = require('role.builder');
+var roleWallRepairer = {
+
+  /** @param {Creep} creep **/
+  run: function(creep) {
+    var wallfound = false;
+    var target;
+    var walls = creep.room.find(FIND_STRUCTURES, {
+      filter: (structure) => {
+        return ((structure.structureType == STRUCTURE_WALL || structure.structureType == STRUCTURE_RAMPART));
+      }
+    });
+
+    for(var percent = .00001; percent < 1;  percent = (percent + .00001) ){
+      for(var i = 0; i < walls.length; i++){
+        if(walls[i].hits < (walls[i].hitsMax * percent)){
+          wallfound = true;
+          target = walls[i];
+          break;
+        }
+      }
+
+      if(wallfound){
+        break;
+      }
+    }
+
+    if (creep.carry.energy === 0){
+      var spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
+      if (creep.room.energyAvailable == creep.room.energyCapacityAvailable){
+        if (spawn.transferEnergy(creep) == ERR_NOT_IN_RANGE){
+          creep.moveTo(spawn);
+        }
+      }
+    }
+    else {
+      if (creep.repair(target) == ERR_NOT_IN_RANGE){
+        creep.moveTo(target);
+      }
+    }
+  }
+};
+
+module.exports = roleWallRepairer;
