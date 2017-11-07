e7d502b6fcc08938409e5c5a61a22b309c0e0c8d
Nibbe <David.Nibbe@securian.com>
2017-11-07 11:28:37 -0600

    fix for turrets

--- main.js
+++ main.js
@@ -127,8 +127,7 @@ module.exports.loop = function () {
     var hostiles = Game.rooms.E22N12.find(FIND_HOSTILE_CREEPS);
     if(hostiles.length > 0) {
         var username = hostiles[0].owner.username;
-        Game.notify(`User ${username} spotted in room ${roomName}`);
-        var towers = Game.rooms[roomName].find(
+        var towers = Game.rooms.E22N12.find(
             FIND_MY_STRUCTURES, {filter: {structureType: STRUCTURE_TOWER}});
         towers.forEach(tower => tower.attack(hostiles[0]));
     }
