a2dac3db6d292902f7cb3ab92aa3aae942339636
Nibbe <David.Nibbe@securian.com>
2017-11-10 13:47:12 -0600

    Testing multi funcitons

--- dispatcher.js
+++ dispatcher.js
@@ -16,10 +16,26 @@ var dispatcher = {
       });
     }
   }
+};
 
-  getConstructionSites: funciton(room){
-    console.log(room);
+var getConstructionSite = {
+  /** @param {Creep} creep **/
+  run: function() {
+    //get all the spawns that I control
+    var spawns = Game.spawns;
+    //we will run the dispatcher on each room we own
+    //(indicated by spawn presence)
+    for (i = 0; i < spawns.length; i++){
+      var spawn = Game.spawns[spawns[i]];
+      var room = spawn.room
+      console.log(room);
+      energytargets = room.find(FIND_MY_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
+        }
+      });
+    }
   }
 };
 
-module.exports = dispatcher;
+module.exports = {dispatcher, getConstructionSite};
